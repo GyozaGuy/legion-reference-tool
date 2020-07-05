ac2307901bc834612bd2493627fbf6393c95f422
GyozaGuy <steven.stromberg@familysearch.org>
2020-07-05 16:08:28 -0600

    Adjusted filter pills

--- public/components/filterPill/templates.mjs
+++ public/components/filterPill/templates.mjs
@@ -7,14 +7,13 @@ export default {
         background-color: White;
         border-radius: 20px;
         display: inline-block;
-        font-size: 2em;
         padding: 5px 10px;
         text-transform: capitalize;
       }
 
       filter-pill[enabled] {
-        background-color: Red;
-        box-shadow: 0 0 8px Red;
+        background-color: #b32e29;
+        box-shadow: 0 0 8px #b32e29;
         color: White;
       }
     </style>
