9275040bf144e88342a9dac56716d3f704f4b1e4
GyozaGuy <gyozaguy@gmail.com>
2020-07-08 00:21:12 -0600

    Greatly improved service worker caching logic

--- public/serviceWorker.mjs
+++ public/serviceWorker.mjs
@@ -1,40 +1,44 @@
 const CACHE_NAME = 'static-cache-v1'
 
-const FILES_TO_CACHE = [
-  '/offline.html'
-]
-
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('[ServiceWorker] Pre-caching offline page')
-      return cache.addAll(FILES_TO_CACHE)
-    })
-  )
-
+self.addEventListener('install', () => {
   self.skipWaiting()
 })
 
-self.addEventListener('activate', () => {
-  caches.keys().then(keyList => {
-    return Promise.all(keyList.map(key => {
+self.addEventListener('activate', async () => {
+  const keyList = await caches.keys()
+  await Promise.all(
+    keyList.map(key => {
       if (key !== CACHE_NAME) {
         console.log('[ServiceWorker] Removing old cache', key)
         return caches.delete(key)
       }
-    }))
-  })
+    })
+  )
 
   self.clients.claim()
 })
 
-self.addEventListener('fetch', evt => {
-  if (evt.request.mode !== 'navigate') {
-    return
-  }
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response
+      }
 
-  evt.respondWith(fetch(evt.request).catch(async () => {
-    const cache = await caches.open(CACHE_NAME)
-    return cache.match('offline.html')
-  }))
+      return fetch(event.request).then(async response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response
+        }
+
+        const responseToCache = response.clone()
+        const cache = await caches.open(CACHE_NAME)
+
+        if (event.request.method !== 'POST') {
+          cache.put(event.request, responseToCache)
+        }
+
+        return response
+      })
+    })
+  )
 })
